58bef477fa707263bec023e967f10fd7b24f56b7
AlekseiKiselev <kisalekseyru37@gmail.com>
2024-04-14 14:03:57 +0800

    fix app routings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Fragment, Suspense, lazy } from "react";
 import { ThemeProvider, StyledEngineProvider, CssBaseline } from "@mui/material";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import theme from "./theme";
 import GlobalStyles from "./GlobalStyles";
 import Pace from "./shared/components/Pace";
 import { StateProvider } from './StateProvider'; 
-import ProtectedRoute from './ProtectedRoute'; // Make sure this import is correct
+import ProtectedRoute from './ProtectedRoute'; 
 
 const LoggedInComponent = lazy(() => import("./logged_in/components/Main"));
 const LoggedOutComponent = lazy(() => import("./logged_out/components/Main"));
@@ -21,13 +21,9 @@ function App() {
             <Pace color={theme.palette.primary.light} />
             <Suspense fallback={<Fragment />}>
               <Switch>
+                <Route exact path="/" component={LoggedOutComponent} />
                 <ProtectedRoute path="/c" component={LoggedInComponent} />
-                <Route exact path="/">
-                  <Redirect to="/c" /> {/* Redirect from home to /c if trying to access the root */}
-                </Route>
-                <Route path="/*">
-                  <LoggedOutComponent />
-                </Route>
+                <Route path="/*" component={LoggedOutComponent} />
               </Switch>
             </Suspense>
           </ThemeProvider>
